43861daa3758bbf895efbe5c10b78599f7f4454f
AVKydin <tlalok@ukr.net>
2022-10-09 21:39:54 +0300

    continue add

--- script.js
+++ script.js
@@ -44,11 +44,11 @@
 //   }
 // }
 
-// function radioColor() {
-//   if (event.target.type === 'radio') {
-//     document.body.style.backgroundColor = event.target.value;
-//   }
-// }
+function radioColor() {
+  if (event.target.type === 'radio') {
+    document.body.style.backgroundColor = event.target.value;
+  }
+}
 
 
 let f4 = document.forms.f4;
@@ -65,7 +65,66 @@ f4.show.addEventListener('click', function () {
     
   // }
   f4.area.style.display = f4.show.checked ? 'block' : 'none';
-  console.log(f4.area.style.display);
-  console.log('f4');
-
+  // console.log(f4.area.style.display);
+  // console.log('f4');
+  // console.log(document.forms.f3.elements[3])
+  document.forms.f3.elements[3].checked = true;
+  console.log(this.value)
 })
+
+let f5 = document.forms.f5;
+
+// console.log(f5.colors);
+// console.log(f5.colors.options);
+// console.log(f5.colors.selectedIndex);
+// console.log(f5.colors.value);
+
+// for (let i = 0; i < f5.colors.options.length; i += 1){
+//   if (f5.colors.options[i].selected) {
+//     console.log(f5.colors.options[i]);
+//   }
+// }
+
+
+// for (let i = 0; i < f5.colors.options.length; i += 1){
+//   f5.colors.addEventListener('change', function () {
+//     if (f5.colors.options[i].selected) {
+//         document.body.style.backgroundColor = f5.colors.options[i].value;
+//     console.log(f5.colors.options[i]);
+//   }
+//   })
+// }
+
+f5.colors.addEventListener('change', function () {
+  document.body.style.backgroundColor = event.target.value;
+})
+
+f5.get.addEventListener('click', function () {
+  // console.log(f5.colors.value)
+  let opt = new Option('gray color', 'gray', true, true);
+  console.log(opt)
+})
+
+let f6 = document.forms.f6;
+
+f6.test.addEventListener("focus", function () {
+  console.log("focus works");
+});
+
+f6.test.addEventListener("blur", function () {
+  console.log("blur works");
+});
+
+f6.test.addEventListener("change", function () {
+  console.log("change works");
+});
+
+f6.test.addEventListener("input", function () {
+  console.log("input works");
+});
+
+f6.test.addEventListener("input", function () {
+  // console.log("input works");
+  document.querySelector('.text').textContent = this.value;
+});
+
